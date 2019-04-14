a7e9d766d3f235b0a13a7bffc9d41174453d4a21
DACUS1995 <studormarian@gmail.com>
2019-04-15 01:23:15 +0300

    now save lstm hidden state

--- models/model_3.py
+++ models/model_3.py
@@ -14,6 +14,9 @@ class Model_3(nn.Module):
 		super().__init__()
 		self.name = "model_3"
 
+		self.h_n = None
+		self.c_n = None
+
 		self.conv_block_1 = nn.Sequential(
 			nn.Conv1d(1, 3, kernel_size=8),
 			nn.MaxPool1d(8),
@@ -48,11 +51,20 @@ class Model_3(nn.Module):
 		out = self.conv_block_1(input)
 		out = self.flatten(out)
 		out = out.view(out.size(0), -1, 1)
-		out, (h_n, c_n) = self.rnn_block_2(out)
 
-		h_n = h_n.view(Config.BATCH_SIZE, LSTM_LAYERS, NUM_DIRECTIONS, HIDDEN_SIZE)
-		h_n = h_n[:, LSTM_LAYERS - 1, :, :]
-		out = h_n.view(h_n.size(0), -1)
+		h_current = None
+		c_current = None
+
+		if self.h_n == None and self.c_n == None:
+			out, (h_current, c_current) = self.rnn_block_2(out)
+		else:
+			out, (h_current, c_current) = self.rnn_block_2(out, (self.h_n, self.c_n))
+		self.h_n = h_current
+		self.c_n = c_current
+
+		out = h_current.view(Config.BATCH_SIZE, LSTM_LAYERS, NUM_DIRECTIONS, HIDDEN_SIZE)
+		out = out[:, LSTM_LAYERS - 1, :, :]
+		out = out.view(out.size(0), -1)
 
 		out = self.linear_block_3(out)
 		return out
